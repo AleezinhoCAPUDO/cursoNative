53c4bdc8c66efc5e65a54dd83b037385b3f88a0e
AleezinhoCAPUDO <alexandrekk21@gmail.com>
2025-05-19 13:50:54 -0300

    Aula 13

---
+++ App.js
@@ -0,0 +1,72 @@
+import React, {useState}  from 'react';
+import { View,
+   Text,
+    StyleSheet,
+     TextInput,
+      Platform,
+     } from 'react-native';
+import { Button } from './src/components/Button';
+import { SkillCard } from './src/components/SkillCard';
+
+export default function App(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill(){
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
+  return (
+    <>
+    <View style={ styles.container }>
+      <Text style={styles.title}>Bem vindo Cuzão</Text>
+    <TextInput 
+      style={styles.input}
+      placeholder='New Skill'
+      placeholderTextColor= '#555'
+      onChangeText={setNewSkill}
+    
+    />
+
+<Button onPress= {handleAddNewSkill}/>
+
+
+
+<Text style={[styles.title, {marginVertical: 50 }]}>
+  My Skills
+</Text>
+
+{
+  mySkills.map(skill =>(
+    <SkillCard skill={skill}/>
+ ))
+}
+
+    </View>
+    </>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+      backgroundColor: '#121015',
+      paddingHorizontal: 20,
+      paddingVertical: 70,
+  },
+    title: {
+      color: '#FFF',
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    input: {
+      backgroundColor: '#1F1e25',
+      color: '#FFF',
+      fontSize: 18,
+      padding: Platform.OS === 'ios' ? 15 : 10,
+      marginTop: 30,
+      borderRadius: 7
+    },
+
+});
---
+++ src/components/SkillCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+export function SkillCard({ skill }) {
+    return (
+         <TouchableOpacity key="skill" style={styles.buttonskill}>
+            <Text style={styles.textskill}>
+            {skill}
+        
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    buttonskill: {
+      backgroundColor: '#1F1e25',
+      padding: 15,
+      borderRadius: 50,
+      alignItems: 'center',
+      marginVertical: 10,
+    },
+
+    textskill: {
+      color: '#FFF',
+      
+      fontSize: 22,
+      fontWeight: 'bold',
+      
+
+    }
+
+});
